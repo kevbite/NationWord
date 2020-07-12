9afa0bb6060dc5e65b2a85867eaa404ff999c0b2
Phil Pursglove <philpursglove@users.noreply.github.com>
2020-07-12 11:28:06 +0100

    Testing for duplicate characters in a string

--- NationWord/NationWord/Program.cs
+++ NationWord/NationWord/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NationWord
 {
@@ -7,6 +8,29 @@ namespace NationWord
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class StringAssembler
+    {
+        public bool CheckForDuplicateLetters(string word)
+        {
+            List<string> letters = new List<string>();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                string letter = word.Substring(i, 1);
+                if (letters.Contains(letter))
+                {
+                    return true;
+                }
+                else
+                {
+                    letters.Add(letter);
+                }
+            }
+
+            return false;
         }
     }
 }
--- NationWord/NationWordTests/Tests.cs
+++ NationWord/NationWordTests/Tests.cs
@@ -1,13 +1,19 @@
+using NationWord;
 using NUnit.Framework;
 
 namespace NationWordTests
 {
     public class StringAssemblyTests
     {
-        [Test]
-        public void Test1()
+        [TestCase("aa", true)]
+        [TestCase("abc", false)]
+        [TestCase("banana", true)]
+        [TestCase("abcba", true)]
+        public void When_String_Contains_Duplicate_Letters_Return_True(string word, bool expected)
         {
-            Assert.Pass();
+            StringAssembler assembler = new StringAssembler();
+            bool result = assembler.CheckForDuplicateLetters(word);
+            Assert.That(result, Is.EqualTo(expected));
         }
     }
 }
