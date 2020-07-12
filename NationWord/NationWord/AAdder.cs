5cf446efbb6ce4c8e1d84e2240ab42869404e1f8
Phil Pursglove <philpursglove@users.noreply.github.com>
2020-07-12 12:51:38 +0100

    Check for aaa already present

---
+++ NationWord/NationWord/AAdder.cs
@@ -0,0 +1,15 @@
+﻿namespace NationWord
+{
+    public class AAdder
+    {
+        public int AddAsTo(string word)
+        {
+            if (word.Contains("aaa"))
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ NationWord/NationWordTests/AAdderTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NationWord;
+using NUnit.Framework;
+
+namespace NationWordTests
+{
+
+    [TestFixture]
+    public class AAdderTests
+    {
+        [TestCase("aaa")]
+        [TestCase("baaa")]
+        public void When_Word_Contains_aaa_Return_Minus_One(string word)
+        {
+            AAdder adder = new AAdder();
+            int result = adder.AddAsTo(word);
+            Assert.That(result, Is.EqualTo(-1));
+        }
+
+
+    }
+}
