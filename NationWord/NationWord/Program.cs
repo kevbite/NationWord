36843e46fbdd0deeeb58d0804542681a387c3ab3
Phil Pursglove <philpursglove@users.noreply.github.com>
2020-07-12 11:21:45 +0100

    Project creation

---
+++ NationWord/NationWord/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NationWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ NationWord/NationWordTests/Tests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace NationWordTests
+{
+    public class StringAssemblyTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
